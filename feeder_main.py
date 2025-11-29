731da0a407de3f4463185a74b0b49790b08ccdfa
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-11-29 16:30:33 +0100

    feeder_main fix

--- feeder_main.py
+++ feeder_main.py
@@ -148,8 +148,13 @@ class BluetoothServer:
 
             port = self.server_sock.getsockname()[1]
 
-            service_classes=[self.uuid]
-
+            bluetooth.advertise_service(
+                self.server_sock,
+                "RaspberryPiFeeder",
+                service_id=self.uuid,
+                service_classes=[self.uuid, bluetooth.SERIAL_PORT_CLASS],
+                profiles=[bluetooth.SERIAL_PORT_PROFILE]
+            )
 
             logging.info(f"Serwer Bluetooth nasłuchuje na porcie {port}")
             logging.info("Czekanie na połączenie...")
